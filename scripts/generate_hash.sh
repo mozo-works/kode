d1939ea86a51f45414fb620ecde95b640328f391
yeongtaek, Hong <mozodev@users.noreply.github.com>
2017-05-12 18:05:42 +0900

    개발 환경을 위한 스크립트 및 설정을 추가합니다.

--- scripts/generate_hash.sh
+++ scripts/generate_hash.sh
@@ -1,1 +1,5 @@
-drush eval "print Drupal\Component\Utility\Crypt::randomBytesBase64(55)"
+# https://gist.github.com/pfaocle/ec6c0c4a65c70f6fb1e1
+drush eval "print Drupal\Component\Utility\Crypt::randomBytesBase64(55); echo PHP_EOL;"
+
+
+# or http://drupalhashsalt.com
